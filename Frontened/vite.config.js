f536f0339656dcf05b31fe442e4f085d206e7b09
Amit Saraswat <saraswatamit227@gmail.com>
2025-07-19 23:34:50 +0530

    updated

--- Frontened/vite.config.js
+++ Frontened/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port:3001,
     proxy:{
       "/api": {
-        target:"http://localhost:4002",
+        target:"https://chatapp-gypf.onrender.com",
         changeOrigin:true,
       }
     }
